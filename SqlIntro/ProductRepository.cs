d6e7b471d09fa58a5d9b0a0131fa5c418a89fc90
Darrell Stovall <darrell.stovall.trussville@gmail.com>
2017-10-24 14:33:17 -0500

    With the help of a classmate, I've updated the coding lines for Insert.

--- SqlIntro/ProductRepository.cs
+++ SqlIntro/ProductRepository.cs
@@ -90,6 +90,6 @@ namespace SqlIntro
                 cmd.ExecuteNonQuery();
             }
          
-            }
+        }
     }
 }
---
+++ SqlIntro/SimpleDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SqlIntro
+{
+    class SimpleDB
+    {
+        private readonly string _connectionString;
+
+        public SimpleDB(string connectionString);
+        {
+            _connectionString = DbConnectionStringBuilder;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Query<Product>();
+            }
+        }
+    }
+}
