a784238c85c1ad286bd0bfdd8b29123b97d2a921
Darrell Stovall <darrell.stovall.trussville@gmail.com>
2017-10-24 14:40:49 -0500

    Deleted my password from the file from last week.

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq;
 
 namespace SqlIntro
@@ -7,7 +8,7 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            var connectionString = "Server=localhost;Database=Adventureworks;Uid=root;Pwd=5Fingers"; //get connectionString format from connectionstrings.com and change to match your database
+            var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString; 
             var repo = new ProductRepository(connectionString);
             foreach (var prod in repo.GetProducts().Take(1))
             {
