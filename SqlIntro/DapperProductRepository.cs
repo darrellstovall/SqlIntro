6af616b3ad3e94ed54c20b3cb8d794308dcb59cb
Darrell Stovall <darrell.stovall.trussville@gmail.com>
2017-10-24 19:51:18 -0500

    Added new class DapperProductRepository, with two new methods to call the inner join and the left join commands.  Not working yet.

---
+++ SqlIntro/DapperProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlIntro
+{
+    public class DapperProductRepository
+    {
+        private readonly string _connectionString;
+
+        public ProductRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                public void GetProductsWithReview(Product prod)
+                {
+                    using (var conn = new MySqlConnection(_connectionString))
+                    {
+                        conn.Open();
+                        conn.Execute("select * from product p inner join productreview pr on p.ProductId = pr.ProductID");
+                    }
+                }
+                public void GetProductsAndReviews(Product prod)
+                {
+                    using (var conn = new MySqlConnection(_connectionString))
+                    {
+                        conn.Open();
+                        conn.Execute("select * from product p left join productreview pr on p.ProductId = pr.ProductID");
+                    }
+                }
+            }
+        }
+    }
+}
